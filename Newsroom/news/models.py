1176cc59a19de081e36f69827f9f23535393bbb9
Surabhi <spd10195@gmail.com>
2020-05-22 16:31:52 -0400

    Store obj info in models.py

--- Newsroom/news/models.py
+++ Newsroom/news/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+#Used to store title, URL of source, and URL of article image
+class Headline(models.Model):
+    title = models.CharField(max_length=200)
+    image = models.URLField(null=True, blank=True)
+    url = models.TextField()
+    
+    #returns the string representation of fetched object
+    def __str__(self):
+        return self.title
